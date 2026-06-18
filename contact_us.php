c19e3f30c6ae3611b30674255d1b22b956076235
sandupa <sghewapathirana@students.nsbm.ac.lk>
2026-06-18 11:59:05 +0530

    /

---
+++ contact_us.php
@@ -0,0 +1,43 @@
+<?php include('header.php') ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+
+<div class="contact_h1"><h1>Contact Us</h1></div><br>
+<div class="contact_h5"><h5>Any question or remarks? Just write us message!</h5></div><br>
+<div class="display_flex">
+    <div class="leftside">
+        <h2 class="contact_h2">Get in touch</h2><br>
+        <div class="contact-item"> <i class="fa-solid fa-location-dot"></i>
+            <p>Mahenwaththa, Pitipana, Homagama, Sri Lanka</p></div><br>
+        <div class="contact-item"> <i class="fa-solid fa-envelope"></i>
+            <p>sandupahewapathirana@gmail.com</p></div><br>
+        <div class="contact-item"> <i class="fa-solid fa-phone"></i>
+            +94 77 659 5069</div><br>
+        <div class="contact-item"> <i class="fa-brands fa-whatsapp"></i>
+            +94 659 5069</div><br>
+        <div class="contact-item"> <i class="fa-brands fa-instagram"></i>
+            nsbm_bazaar</div>
+       
+        
+
+
+
+
+    </div>
+    <div class="rightside">
+        <input type="text" placeholder="Name" class="name"><br>
+        <input type="email" placeholder="Email" class="email"><br>
+        <textarea rows="5" cols="20" placeholder="Message" class="message"></textarea>
+
+    </div>
+
+
+        
+
+</div>
+
+    
+    
+
+
+<?php include('footer.php') ?>
